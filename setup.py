e547947ba5058d4551c35aeaf104c44765ead633
FahimAnayet <fahimanayet@gmail.com>
2021-10-18 08:03:43 +0600

    Gogo added js so, need python, sorry :(

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+setup(
+    name='gogo-cli',
+    version='1.0.0',
+    description='this package is must for gogo-cli bashscript to run',
+    author='Fahim Anayet',
+    url='fahimanayet.github.io/Portfolio',
+    packages=['gogo'],
+    scripts=[
+        'script/gogo-cli'
+    ],
+)
